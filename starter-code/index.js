854c742ed0955ae9ca6360c75495cd476af13b00
rttoshio <rttoshio@hotmail.com>
2023-06-19 16:19:21 -0300

    nav-bar interaction and icon w/ JS

---
+++ starter-code/index.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", () => {
+  // if the nav is closed, open it
+  const visibility = nav.getAttribute("data-visible");
+  // if the nav is open, close it
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
